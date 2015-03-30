b070d1c6ec611131eeb988545c005ac1c0f4ebf2
bparraga20@gmail.com <bparraga20@gmail.com@6c337c50-47de-e475-c768-2e4f06f59612>
2015-03-30 00:11:07 +0000

    Envio realizado con Rest

---
+++ trunk/LocalizaV1/LocalizaV1/IEnvioSVC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using LocalizaV1.Dominio;
+
+namespace LocalizaV1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IEnvioSVC" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IEnvioSVC
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Envios", ResponseFormat = WebMessageFormat.Json)]
+        Envio CrearEnvio(Envio envioACrear);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Envios/{idEnvio}", ResponseFormat = WebMessageFormat.Json)]
+        Envio ObtenerEnvio(string idEnvio);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "Envios", ResponseFormat = WebMessageFormat.Json)]
+        Envio ModificarEnvio(Envio envioAModificar);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "Eliminar/{idEnvio}", ResponseFormat = WebMessageFormat.Json)]
+        string EliminarEnvio(string idEnvio);
+
+        [OperationContract]
+        List<Envio> ListarEnvios();
+    }
+}
---
+++ trunk/LocalizaV1/LocalizaV1/Persistencia/EnvioDAO.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LocalizaV1.Dominio;
+using System.Data.SqlClient;
+
+namespace LocalizaV1.Persistencia
+{
+    public class EnvioDAO
+    {
+        public Envio Crear(Envio envioACrear)
+        {
+            Envio envioCreado = null;
+            string sql = "INSERT INTO t_envio VALUES (@idEnvio, @idCliente, @cantidad, @peso, @destinoInicio, @destinoFin, @idTransporte, @estado)";
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@idEnvio", envioACrear.IdEnvio));
+                    com.Parameters.Add(new SqlParameter("@idCliente", envioACrear.IdCliente));
+                    com.Parameters.Add(new SqlParameter("@cantidad", envioACrear.Cantidad));
+                    com.Parameters.Add(new SqlParameter("@peso", envioACrear.Peso));
+                    com.Parameters.Add(new SqlParameter("@destinoInicio", envioACrear.DestinoInicio));
+                    com.Parameters.Add(new SqlParameter("@destinoFin", envioACrear.DestinoFin));
+                    com.Parameters.Add(new SqlParameter("@idTransporte", envioACrear.IdTransporte));
+                    com.Parameters.Add(new SqlParameter("@estado", envioACrear.Estado));
+                    com.ExecuteNonQuery();
+                }
+
+            }
+            envioCreado = Obtener(envioACrear.IdEnvio);
+            return envioCreado;
+        }
+
+        public Envio Obtener(string idEnvio)
+        {
+            Envio envioEncontrado = null;
+            string sql = "SELECT * FROM t_envio WHERE idEnvio=@idEnvio";
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@idEnvio", idEnvio));
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        if (resultado.Read())
+                        {
+                            envioEncontrado = new Envio()
+                            {
+                                IdEnvio = (string)resultado["idEnvio"],
+                                IdCliente = (string)resultado["idCliente"],
+                                Cantidad = (string)resultado["cantidad"],
+                                Peso = (string)resultado["peso"],
+                                DestinoInicio = (string)resultado["destinoInicio"],
+                                DestinoFin = (string)resultado["destinoFin"],
+                                IdTransporte = (string)resultado["idTransporte"],
+                                Estado = (string)resultado["estado"]
+                            };
+                        }
+                    }
+                }
+            }
+            return envioEncontrado;
+        }
+
+        public Envio Modificar(Envio envioAModificar)
+        {
+            return null;
+        }
+
+        public void Eliminar(string idEnvio)
+        {
+            string sql = "delete from t_envio where idEnvio=@idEnvio";
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@idEnvio", idEnvio));
+                    com.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Envio> ListarTodos()
+        {
+            return null;
+        }
+    }
+
+}
