b1679d771dc4797ebc8ddf01feb8e33850e55988
aless.diaz1 <aless.diaz1@6c337c50-47de-e475-c768-2e4f06f59612>
2015-04-06 17:46:59 +0000

    Servicio de cambio de contraseña con validaciones

---
+++ trunk/LocalizaV1/LocalizaV1/IContraseña.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LocalizaV1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ICambiarContraseña" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IContraseña
+    {
+      [OperationContract]
+        string CambContraseña(string usuario, string nuevaContraseña, string contraseña2);
+
+        [OperationContract]
+        string ValidaContraseñaActual(string usuario, string contraseña);
+    }
+    
+}
